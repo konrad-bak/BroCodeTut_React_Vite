67280d83a343bef1688eed815541f234be75be4b
konrad-bak <bak.konrad@gmail.com>
2024-03-03 19:30:43 +0100

    add ToDoList complete

--- my-react-app/src/ToDoList.jsx
+++ my-react-app/src/ToDoList.jsx
@@ -11,10 +11,34 @@ function ToDoList() {
   function handleInputChange(event) {
     setNewTask(event.target.value);
   }
-  function addTask() {}
-  function deleteTask(index) {}
-  function moveTaskUp(index) {}
-  function moveTaskDown(index) {}
+  function addTask() {
+    setTasks(t => [...t, newTask]);
+    setNewTask("");
+  }
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
+  function moveTaskDown(index) {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [
+        updatedTasks[index + 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
 
   return (
     <div className="to-do-list">
